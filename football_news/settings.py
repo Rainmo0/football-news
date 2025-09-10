8c23ac5cea1d99abcc9a77e014a6221aaa87cd9c
Rainmo0 <aunknown145@gmail.com>
2025-09-10 14:20:27 +0700

    tutorial 2 selesai yaya

--- football_news/settings.py
+++ football_news/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'football_news.wsgi.application'
 if PRODUCTION:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
+            'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.getenv('DB_NAME'),
             'USER': os.getenv('DB_USER'),
             'PASSWORD': os.getenv('DB_PASSWORD'),
